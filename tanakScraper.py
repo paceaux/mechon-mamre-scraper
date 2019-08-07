f989a5c692383815eb26bbb33fc9ab3d73a9defd
Frank Taylor <Frank.Taylor@exlrt.com>
2019-08-07 16:28:46 -0500

    adds ability to get list of all books in tanakh

---
+++ tanakScraper.py
@@ -0,0 +1,30 @@
+import urllib, os, sys, getopt, json, collections
+from fetchPages import simple_get
+from tanakhData import get_tanakh_data
+
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, 'u:', ['url='])
+    except getopt.GetoptError:
+        print('error with the arguments')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-u', '--url'):
+            url = arg
+
+    try:
+        tanakh_data = get_tanakh_data(url)
+        file_name = 'tanakhBookLinks.json'
+
+        with open(file_name, 'w') as outfile:
+            json.dump(tanakh_data, outfile, indent=4)
+
+    except Exception as e:
+        print('Error With outputting the file')
+        print(e)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ tanakhData.py
@@ -0,0 +1,51 @@
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+import urllib, os, sys, getopt, json, collections, re
+from fetchPages import get_page
+
+def get_links_from_start(startPoint, prepended_path = ''):
+    linkList = {}
+    for element in startPoint.next_siblings:
+        if (element.name == 'br'):
+            break
+        if (element.name == 'a'):
+            linkList[element.text] = prepended_path + element['href']
+    return linkList
+
+def get_tanakh_data_from_html(pageHtml, url):
+    full_path = get_path_for_urls(url);
+    torahListStart = pageHtml.find('strong', text=re.compile('Torah'))
+    neviimListStart = pageHtml.find('strong', text=re.compile('Prophets'))
+    kethuvimListStart = pageHtml.find('strong', text=re.compile('Writings'))
+
+    torahList = get_links_from_start(torahListStart, prepended_path = full_path)
+    neviimList = get_links_from_start(neviimListStart, prepended_path = full_path)
+    kethuvimList = get_links_from_start(kethuvimListStart, prepended_path = full_path)
+
+    return {'torah': torahList, 'prophets': neviimList, 'writings': kethuvimList }
+
+def get_current_page(url):
+    url_parts = url.split('/')
+    url_parts_len = len(url_parts)
+
+    return url_parts[url_parts_len - 1]
+
+def get_path_for_urls(url):
+    current_page = get_current_page(url)
+    path_for_urls = url.replace(current_page, '')
+
+    return path_for_urls
+
+def get_tanakh_data(url):
+    print(url)
+    try:
+        pageHtml = get_page(url)
+        tanakh_data = get_tanakh_data_from_html(pageHtml, url)
+        tanakh_data['startUrl'] = url
+
+        return tanakh_data
+
+    except Exception as error:
+        print(error)
+
