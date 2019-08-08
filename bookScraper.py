e1a68c76ce3b2a9ddfac9dec08b9bc58bba0c2a0
Frank Taylor <Frank.Taylor@exlrt.com>
2019-08-07 21:20:37 -0500

    creates ability to scrape from another file

--- bookScraper.py
+++ bookScraper.py
@@ -5,6 +5,18 @@ from bookData import get_book_data
 
 OUTPUT_DIRECTORY = 'books'
 
+def scrapeBook(url):
+    book_data = get_book_data(url)
+    book_file_name = OUTPUT_DIRECTORY + '/' + book_data['canonicalBookName'] + '.json'
+
+    for chapter_num, chapter_link in book_data['chapters'].items():
+        chapter_data = get_chapter_data(chapter_link)
+        chapter_data['link'] = chapter_link
+        book_data['chapters'][chapter_num] = chapter_data
+
+    with open(book_file_name, 'w') as outfile:
+        json.dump(book_data, outfile, indent=4)
+
 def main(argv):
     try:
         opts, args = getopt.getopt(argv, 'u:', ['url='])
@@ -17,17 +29,7 @@ def main(argv):
             url = arg
 
     try:
-        book_data = get_book_data(url)
-        book_file_name = OUTPUT_DIRECTORY + '/' + book_data['canonicalBookName'] + '.json'
-
-
-        for chapter_num, chapter_link in book_data['chapters'].items():
-            chapter_data = get_chapter_data(chapter_link)
-            chapter_data['link'] = chapter_link
-            book_data['chapters'][chapter_num] = chapter_data
-
-        with open(book_file_name, 'w') as outfile:
-            json.dump(book_data, outfile, indent=4)
+        scrapeBook(url)
 
     except Exception as e:
         print('Error With outputting the file')
---
+++ scrapeAllBooks.py
@@ -0,0 +1,36 @@
+import urllib, os, sys, getopt, json, collections
+from fetchPages import simple_get
+from bookScraper import scrapeBook
+
+OUTPUT_DIRECTORY = 'books'
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, 'g:b:', ['g=', 'b='])
+    except getopt.GetoptError:
+        print('error with the arguments')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-g', '--group'):
+            bookGroupName = arg
+        if opt in ('-b', '--book'):
+            bookName = arg
+
+    try:
+        with open('books/tanakhBookLinks.json') as datafile:
+            bookList = json.load(datafile)
+
+            if (bookList[bookGroupName] is not None):
+                print(bookList[bookGroupName])
+                for book, url in bookList[bookGroupName].items():
+                    print(url)
+                    scrapeBook(url)
+
+    except Exception as e:
+        print('Error With outputting the file')
+        print(e)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
